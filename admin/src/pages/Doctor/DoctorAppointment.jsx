edfd2ceace299a81eb1106e4eb22d162c711e5ae
Aanchallll18 <kumariaachal002@gmail.com>
2025-01-23 21:06:33 +0530

    commit

--- admin/src/pages/Doctor/DoctorAppointment.jsx
+++ admin/src/pages/Doctor/DoctorAppointment.jsx
@@ -1,38 +1,12 @@
-// import React, { useContext, useEffect } from 'react'
-// import { DoctorContext } from '../../context/DoctorContext'
 
-// const DoctorAppointment = () => {
-//     const {dToken,appointments,getAppointments}=useContext(DoctorContext)
-
-//     useEffect(()=>{
-//         if(dToken){
-//             getAppointments()
-//         }
-//     },[dToken])
-
-//   return (
-//     <div className='w-full max-w-6xl m-5'>
-
-//       <p className='bg-white border rounded text-sm max-h-[80vh] min-h-[50vh] overflow-y-scroll '>All Appointments</p>
-//         <div className='max-sm:hidden grid grid-cols-[0.5fr_2fr_1fr_3fr_1fr_1fr] gap-1 py-3 px-6 border-b'>
-//             <p>#</p>
-//             <p>Patient</p>
-//             <p>Payment</p>
-//             <p>Age</p>
-//             <p>Date & Time</p>
-//             <p>Fees</p>
-//             <p>Action</p>
-//         </div>
-//     </div>
-//   )
-// }
-
-// export default DoctorAppointment
 import React, { useContext, useEffect } from 'react';
 import { DoctorContext } from '../../context/DoctorContext';
+import { AppContext } from '../../context/AppContext';
+import { assets } from '../../assets/assets';
 
 const DoctorAppointment = () => {
   const { dToken, appointments, getAppointments } = useContext(DoctorContext);
+  const {calculateAge,curreny}=useContext(AppContext)
 
   useEffect(() => {
     if (dToken) {
@@ -62,22 +36,31 @@ const DoctorAppointment = () => {
                 key={item.id}
                 className="grid grid-cols-[0.5fr_2fr_1fr_1fr_1.5fr_1fr_1fr] gap-4 items-center py-3 px-6 border-b hover:bg-gray-50"
               >
-                <p>{index + 1}</p>
+                <p className='max-sm:hidden'>{index + 1}</p>
                 <div>
-                    <img src={item.userData.image} alt="" />
+                    <img 
+                    className="w-12 h-12 rounded-full"
+                    src={item.userData.image} alt="" />
                     <p>{item.userData.name}</p>
                 </div>
                 <div>
-                    <p>
+                    <p className='text-sm inline border border-purple-300 px-2 rounded-full'>
                         {item.payment ? 'Online' : 'Cash'}
                     </p>
                 </div>
-                <p>
-                    
+                <p className='max-sm:hidden'>
+                    {calculateAge(item.userData.DOB)}
                 </p>
-                <button className="text-red-500  border border-red-600 px-4 py-1 rounded-lg">
-                  Cancel
-                </button>
+                <p>{item.slotTime}</p>
+                <p>{curreny}{item.amount}</p>
+                <div>
+                    <img
+                    className='w-10 cursor-pointer'
+                     src={assets.cancel_icon} alt="" />
+                    <img 
+                    className='w-10 cursor-pointer'
+                    src={assets.tick_icon} alt="" />
+                </div>
               </div>
             ))
           ) : (
