b886544bc5437bd341647feed8a715ca8a85fa21
Aanchallll18 <kumariaachal002@gmail.com>
2025-01-23 18:08:42 +0530

    commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,6 +11,9 @@ import Appointment from './pages/Admin/Appointment';
 import Doctorlist from './pages/Admin/Doctorlist';
 import Dashboard from './pages/Admin/Dashboard';
 import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointment from './pages/Doctor/DoctorAppointment';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
 
 const App = () => {
   const {aToken}=useContext(AdminContext)
@@ -34,6 +37,13 @@ const App = () => {
         <Route path='/add-doctor' element={<AddDoctor/>}></Route>
 
         <Route path='/doctor-list' element={<Doctorlist/>}></Route>
+
+        {/* doctor Route */}
+        <Route path='/doctor-dashboard' element={<DoctorDashboard/>}></Route>
+
+        <Route path='/doctor-appointment' element={<DoctorAppointment/>}></Route>
+
+        <Route path='/doctor-profile' element={<DoctorProfile/>}></Route>
 
       </Routes>
       </div>
