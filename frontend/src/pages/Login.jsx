04818b0d9c95f92171cf2a05c6708e281348c269
Aanchallll18 <kumariaachal002@gmail.com>
2025-01-24 21:05:07 +0530

    commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 
 import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from '../context/AppContext';
-import toast from 'react-hot-toast'
+import {toast} from 'react-hot-toast'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
