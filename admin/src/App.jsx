a5e5434b1ba9ec74626e6b84663000d4fa9a89b9
Aanchallll18 <kumariaachal002@gmail.com>
2025-02-02 10:36:31 +0530

    commit

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useContext } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login'
-import toast from 'react-hot-toast';
+
+import React, { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
 import AddDoctor from './pages/Admin/AddDoctor';
 import Appointment from './pages/Admin/Appointment';
 import Doctorlist from './pages/Admin/Doctorlist';
@@ -16,45 +17,34 @@ import DoctorAppointment from './pages/Doctor/DoctorAppointment';
 import DoctorProfile from './pages/Doctor/DoctorProfile';
 
 const App = () => {
-  const {aToken}=useContext(AdminContext)
-  const {dToken}=useContext(DoctorContext)
+  const { aToken } = useContext(AdminContext) || {};
+  const { dToken } = useContext(DoctorContext) || {};
 
   return aToken || dToken ? (
-    <div >
-     <ToastContainer/>
-     <Navbar/>
-     <div className='flex items-start'>
-      <Sidebar></Sidebar>
-
-      <Routes>
-
-        <Route path='/' element={<></>}></Route>
-
-        <Route path='/admin-dashboard' element={<Dashboard/>}></Route>
-
-        <Route path='/all-appointments' element={<Appointment/>}></Route>
-
-        <Route path='/add-doctor' element={<AddDoctor/>}></Route>
-
-        <Route path='/doctor-list' element={<Doctorlist/>}></Route>
-
-        {/* doctor Route */}
-        <Route path='/doctor-dashboard' element={<DoctorDashboard/>}></Route>
-
-        <Route path='/doctor-appointment' element={<DoctorAppointment/>}></Route>
-
-        <Route path='/doctor-profile' element={<DoctorProfile/>}></Route>
-
-      </Routes>
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<Appointment />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<Doctorlist />} />
+          {/* Doctor Routes */}
+          <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+          <Route path="/doctor-appointment" element={<DoctorAppointment />} />
+          <Route path="/doctor-profile" element={<DoctorProfile />} />
+        </Routes>
       </div>
-
     </div>
   ) : (
     <>
-    <Login/>
-    <ToastContainer/>
+      <Login />
+      <ToastContainer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
